096ecf321abe7f87c8790f8d5f350f66ddfa16f5
Himanshu Goel <himanshu@hgoel.dev>
2020-08-23 14:33:38 -0400

    decode optimizations

--- rle.c
+++ rle.c
@@ -1,24 +1,43 @@
 #include "rle.h"
+#include <string.h>
 #include <x86intrin.h>
 
-uint32_t naive_rle(uint64_t *raw_Data_u64, uint32_t len, uint8_t *dst){
-    uint8_t *raw_Data = (uint8_t*)raw_Data_u64;
+uint32_t naive_rle(uint64_t *raw_Data_u64, uint32_t len, uint8_t *dst)
+{
+    uint8_t *raw_Data = (uint8_t *)raw_Data_u64;
 
     uint8_t *init_dst = dst;
-    uint8_t cur_val = raw_Data[0];
-    uint32_t cur_run = 1;
-    for (int i = 1; i < len; i++){
-        if((raw_Data[i] != cur_val) | (cur_run == 256)){
-            *(dst++) = (cur_run - 1);
-            *(dst++) = cur_val;
+    uint8_t cur_val = *(raw_Data++);
+    uint8_t cur_run = 0;
+    for (int i = 1; i < len; i++)
+    {
+        if ((*raw_Data != cur_val) | (cur_run == 255))
+        {
+            dst[0] = cur_run;
+            dst[1] = cur_val;
+            dst += 2;
 
-            cur_val = raw_Data[i];
-            cur_run = 0;
+            cur_val = *raw_Data;
+            cur_run = -1;
         }
-            cur_run++;
+        cur_run++;
+        raw_Data++;
     }
 
     return (dst - init_dst);
+}
+
+uint32_t entropy_rle(uint8_t *raw_Data_u64, uint32_t len, uint8_t *dst)
+{
+    uint32_t counts[256];
+    memset(counts, 0, 256 * sizeof(uint32_t));
+    for (int i = 0; i < len; i++)
+        counts[raw_Data_u64[i]]++;
+    uint32_t max_idx = 0;
+    for (int i = 1; i < 256; i++)
+        if (counts[i] > counts[max_idx])
+            max_idx = i;
+    return -1;
 }
 
 uint32_t rle_encode(uint64_t *raw_Data_u64, uint32_t len, uint8_t *dst)
@@ -27,31 +46,32 @@ uint32_t rle_encode(uint64_t *raw_Data_u64, uint32_t len, uint8_t *dst)
     uint64_t next_val = raw_Data_u64[0];
     uint32_t c_runLen = 0;
     uint32_t c_val = next_val & 0xff;
-    uint64_t *comp_ptr = (uint64_t *)dst;
+    uint8_t *comp_ptr = (uint8_t *)dst;
     uint64_t comp_str = 0;
     uint32_t comp_str_cntr = 0;
 
     uint64_t *comp_ptr_start = comp_ptr;
     for (int j = 1; j < len / sizeof(uint64_t); j++)
     {
+        //load these into vector registers
+        //shift vector by 1 byte
+        //blendv
+        //xor
+        //movemask
+        //tzcnt trick
         cur_val = next_val;
         next_val = raw_Data_u64[j];
-        uint64_t cur_val_shifted = (cur_val >> 8) | (next_val << 56);
-        uint64_t equality_mask = cur_val_shifted ^ cur_val;
-        //count matched parts
-        if (equality_mask == 0)
+        uint64_t proc_muld = (uint8_t)cur_val * 0x0101010101010101;
+        if (proc_muld == cur_val)
         {
             uint8_t proc_byte = cur_val;
             if (c_val == proc_byte && c_runLen + 64 <= 256 * 8)
                 c_runLen += 64;
             else
             {
-                comp_str_cntr++;
-                comp_str = (comp_str << 16) | (c_val << 8) | (c_runLen / 8 - 1);
-                if ((comp_str_cntr & 3) == 0)
-                    *(comp_ptr++) = comp_str;
-                //*(comp_ptr++) = c_runLen / 8 - 1;
-                //*(comp_ptr++) = c_val;
+                comp_ptr[0] = c_runLen / 8 - 1;
+                comp_ptr[1] = c_val;
+                comp_ptr += 2;
 
                 c_runLen = 64;
                 c_val = proc_byte;
@@ -59,6 +79,10 @@ uint32_t rle_encode(uint64_t *raw_Data_u64, uint32_t len, uint8_t *dst)
         }
         else
         {
+            uint64_t cur_val_shifted = (cur_val >> 8) | (next_val << 56);
+            uint64_t equality_mask = cur_val_shifted ^ cur_val;
+            //count matched parts
+
             int32_t avail_bits = 64;
             for (; avail_bits > 0;)
             {
@@ -72,11 +96,9 @@ uint32_t rle_encode(uint64_t *raw_Data_u64, uint32_t len, uint8_t *dst)
                     c_runLen += matched_bits;
                 else
                 {
-                    comp_str_cntr++;
-                    comp_str = (comp_str << 16) | (c_val << 8) | (c_runLen / 8 - 1);
-                    if ((comp_str_cntr & 3) == 0)
-                        *(comp_ptr++) = comp_str;
-
+                    comp_ptr[0] = c_runLen / 8 - 1;
+                    comp_ptr[1] = c_val;
+                    comp_ptr += 2;
                     c_runLen = matched_bits;
                     c_val = proc_byte;
                 }
@@ -99,10 +121,9 @@ uint32_t rle_encode(uint64_t *raw_Data_u64, uint32_t len, uint8_t *dst)
             c_runLen += 64;
         else
         {
-            comp_str_cntr++;
-            comp_str = (comp_str << 16) | (c_val << 8) | (c_runLen / 8 - 1);
-            if ((comp_str_cntr & 3) == 0)
-                *(comp_ptr++) = comp_str;
+            comp_ptr[0] = c_runLen / 8 - 1;
+            comp_ptr[1] = c_val;
+            comp_ptr += 2;
             //last write, no need to save values further
             c_runLen = 64;
             c_val = proc_byte;
@@ -123,10 +144,9 @@ uint32_t rle_encode(uint64_t *raw_Data_u64, uint32_t len, uint8_t *dst)
                 c_runLen += matched_bits;
             else
             {
-                comp_str_cntr++;
-                comp_str = (comp_str << 16) | (c_val << 8) | (c_runLen / 8 - 1);
-                if ((comp_str_cntr & 3) == 0)
-                    *(comp_ptr++) = comp_str;
+                comp_ptr[0] = c_runLen / 8 - 1;
+                comp_ptr[1] = c_val;
+                comp_ptr += 2;
 
                 c_runLen = matched_bits;
                 c_val = proc_byte;
@@ -139,27 +159,25 @@ uint32_t rle_encode(uint64_t *raw_Data_u64, uint32_t len, uint8_t *dst)
     }
     if (c_runLen != 0)
     {
-        comp_str_cntr++;
-        comp_str = (comp_str << 16) | (c_val << 8) | (c_runLen / 8 - 1);
-        if ((comp_str_cntr & 3) == 0)
-            *(comp_ptr++) = comp_str;
-    }
-    if((comp_str_cntr & 3) != 0){
-        comp_str <<= (16 * (-comp_str_cntr & 3));
-        *(comp_ptr++) = comp_str;
-        comp_str_cntr = (comp_str_cntr + 3) & ~3;
+        comp_ptr[0] = c_runLen / 8 - 1;
+        comp_ptr[1] = c_val;
+        comp_ptr += 2;
     }
 
-    return comp_str_cntr * 2;
+    return (comp_ptr - dst);
 }
 
 uint32_t rle_decode(uint64_t *comp_data_space, uint32_t rle_len, uint8_t *decomp_pool)
 {
     uint8_t *tmp_ptr = (uint8_t *)decomp_pool;
     uint64_t *comp_ptr = (uint64_t *)comp_data_space;
+    uint8_t *comp_ptr_8 = (uint8_t *)comp_data_space;
+    uint64_t *decomp_64 = (uint64_t *)decomp_pool;
     uint32_t iter = 0;
     uint64_t last_write = 0;
 
+    //build 64 bit values and write them in one go
+    //(iter & 7) << 3 | (len & 7)
     const uint64_t mask_lut[] = {
         0xffffffffffffffff,
         0xffffffffffffff00,
@@ -173,39 +191,76 @@ uint32_t rle_decode(uint64_t *comp_data_space, uint32_t rle_len, uint8_t *decomp
 
     for (int j = 0; j < rle_len / 8; j++)
     {
-        uint64_t v_net = *(comp_ptr++);
-
+        uint64_t *tmp_ptr_64;
         int32_t len;
         uint8_t v;
         uint64_t v_64;
-        uint64_t *tmp_ptr_64;
         uint64_t mask;
+        uint64_t iter_tmp;
         uint64_t v_64_s;
-        uint32_t iter_tmp;
 
         //Align the value being written so the pointer can be aligned
         //std::cout << "len: " << std::dec << len << " v: " << v << " iter: " << iter << std::hex << " v_64_s: " << v_64_s << std::endl;
-#define INNER_LOOP(off)                                 \
-    len = ((v_net >> (off * 8)) & 0xff) + 1;            \
-    v = (v_net >> ((off + 1) * 8)) & 0xff;              \
-    v_64 = v * 0x0101010101010101;                      \
-    tmp_ptr_64 = (uint64_t *)&tmp_ptr[iter & ~7];       \
-    iter_tmp = iter & 0x7;\
-    mask = mask_lut[iter_tmp];                      \
-    v_64_s = last_write ^( (last_write ^ v_64) & mask);\
-    last_write = v_64_s;                                \
-    *(tmp_ptr_64++) = v_64_s;                           \
-    iter += len;                                        \
-    len = (len - (8 - iter_tmp) + 7) / 8;                                \
-    if (len > 0)                                        \
-        last_write = v_64;                              \
-    while (len-- > 0)                                   \
-        *(tmp_ptr_64++) = v_64;
+#define INNER_LOOP(off)                                                             \
+    len = comp_ptr_8[off];                                                          \
+    v = comp_ptr_8[(off + 1)];                                                      \
+    if ((iter & 0xf) == 0)                                                          \
+    {                                                                               \
+        tmp_ptr_64 = (uint64_t *)&tmp_ptr[iter];                                    \
+        iter += len + 1;                                                            \
+        len /= 16;                                                                  \
+        __m128i c16 = _mm_set_epi8(v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v); \
+        while (len-- >= 0)                                                          \
+        {                                                                           \
+            _mm_store_si128((__m128i *)tmp_ptr_64, c16);                            \
+            tmp_ptr_64 += 2;                                                        \
+        }                                                                           \
+    }                                                                               \
+    else                                                                            \
+    {                                                                               \
+        v_64 = v * 0x0101010101010101;                                              \
+        tmp_ptr_64 = (uint64_t *)&tmp_ptr[iter & ~7];                               \
+        iter_tmp = iter & 0x7;                                                      \
+        mask = mask_lut[iter_tmp];                                                  \
+        v_64_s = (*(tmp_ptr_64) & ~mask) | (v_64 & mask);                           \
+        *(tmp_ptr_64++) = v_64_s;                                                   \
+        iter += len + 1;                                                            \
+        len = (len - (7 - iter_tmp) + 7) / 8;                                       \
+        while (len-- > 0)                                                           \
+            *(tmp_ptr_64++) = v_64;                                                 \
+    }
 
-        INNER_LOOP(6)
-        INNER_LOOP(4)
-        INNER_LOOP(2)
         INNER_LOOP(0)
+        INNER_LOOP(2)
+        INNER_LOOP(4)
+        INNER_LOOP(6)
+        comp_ptr_8 += 8;
+        //INNER_LOOP(2)
+        //INNER_LOOP(4)
+        //INNER_LOOP(6)
+    }
+
+    rle_len = rle_len & 0xf;
+    if (rle_len > 0)
+    {
+        uint64_t *tmp_ptr_64;
+        int32_t len;
+        uint8_t v;
+        uint64_t v_64;
+        uint64_t mask;
+        uint64_t iter_tmp;
+        uint64_t v_64_s;
+
+        INNER_LOOP(0);
+
+        if (rle_len > 2)
+        {
+            INNER_LOOP(2);
+        }
+        if (rle_len > 4)
+        {
+            INNER_LOOP(4);
+        }
     }
     //for (int j = 0; j < 64 * 1024; j++)
     //    if (decomp_pool[j] != raw_Data[j])
--- test.c
+++ test.c
@@ -3,69 +3,72 @@
 #include <stdlib.h>
 #include <time.h>
 
-void main(int argc, char *argv[]) {
-  FILE *f = fopen(argv[1], "rb");
-  fseek(f, 0, SEEK_END);
-  uint32_t pos = ftell(f);
-  fseek(f, 0, SEEK_SET);
+void main(int argc, char *argv[])
+{
+    FILE *f = fopen(/*argv[1]*/ "chnk_raw.bin", "rb");
+    fseek(f, 0, SEEK_END);
+    uint32_t pos = ftell(f);
+    fseek(f, 0, SEEK_SET);
 
-  uint8_t *src_pool = calloc((pos + 16), 1);
-  uint8_t *dst_pool = calloc((pos + 16) * 4, 1);
-  uint8_t *dec_pool = calloc((pos + 16), 1);
+    uint8_t *src_pool = calloc((pos + 16), 1);
+    uint8_t *dst_pool = calloc((pos + 16) * 4, 1);
+    uint8_t *dec_pool = calloc((pos + 16), 1);
 
-  fread(src_pool, 1, pos, f);
-  fclose(f);
+    fread(src_pool, 1, pos, f);
+    fclose(f);
 
     pos = (pos + 7) & ~7;
+    printf("Source Len: %d \r\n", pos);
 
-    uint32_t expected_len = naive_rle((uint64_t*)src_pool, pos, dst_pool);
+    uint32_t expected_len = naive_rle((uint64_t *)src_pool, pos, dst_pool);
+    printf("Expected Len: %d (%f%%) \r\n", expected_len,
+           (expected_len / (float)pos) * 100);
 
-  uint32_t encoded_len = rle_encode((uint64_t *)src_pool, pos, dst_pool);
-  rle_decode((uint64_t *)dst_pool, encoded_len, dec_pool);
+    uint32_t encoded_len = rle_encode((uint64_t *)src_pool, pos, dst_pool);
+    printf("Encoded Len: %d (%f%%) \r\n", encoded_len,
+           (encoded_len / (float)pos) * 100);
+    fflush(stdout);
 
-  for (uint32_t i = 0; i < pos; i++)
-    if (src_pool[i] != dec_pool[i]) {
-      printf("Mismatch at [%d] = %d should be %d\r\n", i, dec_pool[i],
-             src_pool[i]);
+    f = fopen("out.bin", "wb");
+    fwrite(dst_pool, 1, encoded_len, f);
+    fclose(f);
 
-      f = fopen("out.bin", "wb");
-      fwrite(dst_pool, 1, encoded_len, f);
-      fclose(f);
-      break;
-    }
+    rle_decode((uint64_t *)dst_pool, encoded_len, dec_pool);
+    for (uint32_t i = 0; i < pos; i++)
+        if (src_pool[i] != dec_pool[i])
+        {
+            printf("Mismatch at [%d] = %d should be %d\r\n", i, dec_pool[i],
+                   src_pool[i]);
 
-  printf("Source Len: %d \r\n", pos);
-  printf("Encoded Len: %d (%f%%) \r\n", encoded_len,
-         (encoded_len / (float)pos) * 100);
-  printf("Expected Len: %d (%f%%) \r\n", expected_len,
-         (expected_len / (float)pos) * 100);
+            break;
+        }
 
-  struct timespec tstart;
-  struct timespec tstop;
-  uint32_t iter_cnt = 10;
+    struct timespec tstart;
+    struct timespec tstop;
+    uint32_t iter_cnt = 100000;
 
-  clock_gettime(CLOCK_REALTIME, &tstart);
-  for (int i = 0; i < iter_cnt; i++)
-    rle_encode((uint64_t *)src_pool, pos, dst_pool);
-  clock_gettime(CLOCK_REALTIME, &tstop);
+    clock_gettime(CLOCK_REALTIME, &tstart);
+    for (int i = 0; i < iter_cnt; i++)
+        rle_encode((uint64_t *)src_pool, pos, dst_pool);
+    clock_gettime(CLOCK_REALTIME, &tstop);
 
-  uint64_t start =
-      (uint64_t)tstart.tv_sec * 1000000000L + (uint64_t)tstart.tv_nsec;
-  uint64_t stop =
-      (uint64_t)tstop.tv_sec * 1000000000L + (uint64_t)tstop.tv_nsec;
+    uint64_t start =
+        (uint64_t)tstart.tv_sec * 1000000000L + (uint64_t)tstart.tv_nsec;
+    uint64_t stop =
+        (uint64_t)tstop.tv_sec * 1000000000L + (uint64_t)tstop.tv_nsec;
 
-  double us = (stop - start) / (1000.0 * iter_cnt);
-  double mbps = pos / us;
-  printf("Encode time: %f MB/s\r\n", mbps);
+    double us = (stop - start) / (1000.0 * iter_cnt);
+    double mbps = pos / us;
+    printf("Encode time: %f MB/s (%f us)\r\n", mbps, us);
 
-  clock_gettime(CLOCK_REALTIME, &tstart);
-  for (int i = 0; i < iter_cnt; i++)
-    rle_decode((uint64_t *)dst_pool, encoded_len, src_pool);
-  clock_gettime(CLOCK_REALTIME, &tstop);
+    clock_gettime(CLOCK_REALTIME, &tstart);
+    for (int i = 0; i < iter_cnt; i++)
+        rle_decode((uint64_t *)dst_pool, encoded_len, src_pool);
+    clock_gettime(CLOCK_REALTIME, &tstop);
 
-  start = (uint64_t)tstart.tv_sec * 1000000000L + (uint64_t)tstart.tv_nsec;
-  stop = (uint64_t)tstop.tv_sec * 1000000000L + (uint64_t)tstop.tv_nsec;
-  us = (stop - start) / (1000.0 * iter_cnt);
-  mbps = pos / us;
-  printf("Decode time: %f MB/s\r\n", mbps);
+    start = (uint64_t)tstart.tv_sec * 1000000000L + (uint64_t)tstart.tv_nsec;
+    stop = (uint64_t)tstop.tv_sec * 1000000000L + (uint64_t)tstop.tv_nsec;
+    us = (stop - start) / (1000.0 * iter_cnt);
+    mbps = pos / us;
+    printf("Decode time: %f MB/s (%f us)\r\n", mbps, us);
 }
