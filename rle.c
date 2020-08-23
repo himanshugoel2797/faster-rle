e1b34b8d8d7d00235dd3389c42fff7cfd4b097de
Himanshu Goel <himanshu.goel2797@gmail.com>
2020-08-23 17:31:36 -0400

    AVX2 optimizations for encoder.

--- rle.c
+++ rle.c
@@ -42,126 +42,66 @@ uint32_t entropy_rle(uint8_t *raw_Data_u64, uint32_t len, uint8_t *dst)
 
 uint32_t rle_encode(uint64_t *raw_Data_u64, uint32_t len, uint8_t *dst)
 {
-    uint64_t cur_val = 0;
-    uint64_t next_val = raw_Data_u64[0];
+    __m256i cur_val;
+    __m256i next_val = _mm256_load_si256((__m256i*)&raw_Data_u64[0]);
     uint32_t c_runLen = 0;
-    uint32_t c_val = next_val & 0xff;
+    uint32_t c_val = _mm256_extract_epi8(next_val, 0);
     uint8_t *comp_ptr = (uint8_t *)dst;
     uint64_t comp_str = 0;
     uint32_t comp_str_cntr = 0;
 
-    uint64_t *comp_ptr_start = comp_ptr;
-    for (int j = 1; j < len / sizeof(uint64_t); j++)
+    uint8_t *raw_Data = (uint8_t*)raw_Data_u64;
+    raw_Data += 32;
+    for (int j = 1; j < len / (sizeof(uint64_t) * 4); j++)
     {
-        //load these into vector registers
-        //shift vector by 1 byte
-        //blendv
-        //xor
-        //cmpeq
-        //movemask
-        //not
-        //tzcnt trick
         cur_val = next_val;
-        next_val = raw_Data_u64[j];
-        uint64_t proc_muld = (uint8_t)cur_val * 0x0101010101010101;
-        if (proc_muld == cur_val)
-        {
-            uint8_t proc_byte = cur_val;
-            if (c_val == proc_byte && c_runLen + 64 <= 256 * 8)
-                c_runLen += 64;
-            else
-            {
-                comp_ptr[0] = c_runLen / 8 - 1;
-                comp_ptr[1] = c_val;
-                comp_ptr += 2;
-
-                c_runLen = 64;
-                c_val = proc_byte;
-            }
-        }
-        else
-        {
-            uint64_t cur_val_shifted = (cur_val >> 8) | (next_val << 56);
-            uint64_t equality_mask = cur_val_shifted ^ cur_val;
-            //count matched parts
-
-            int32_t avail_bits = 64;
+        next_val = _mm256_load_si256((__m256i*)&raw_Data_u64[j * 4]);
+        __m256i cur_val_shifted = _mm256_alignr_epi8(_mm256_permute2x128_si256(cur_val, cur_val, _MM_SHUFFLE(2, 0, 0, 1)), cur_val, 1);//_mm256_bsrli_epi128(cur_val, 1);
+        cur_val_shifted = _mm256_insert_epi8(cur_val_shifted, raw_Data[0], 31);
+        __m256i equality_mask_vec = _mm256_xor_si256(cur_val_shifted, cur_val);
+        equality_mask_vec = _mm256_cmpeq_epi8(equality_mask_vec, _mm256_setzero_si256());
+        uint32_t equality_mask = ~_mm256_movemask_epi8(equality_mask_vec);
+            
+            int32_t avail_bits = 32;
             for (; avail_bits > 0;)
             {
-                uint8_t proc_byte = cur_val;
-                int32_t matched_bits = __tzcnt_u64(equality_mask) & ~7;
-                matched_bits += 8;
-                if (matched_bits > avail_bits)
-                    matched_bits = avail_bits;
+                uint32_t zero_bits = __tzcnt_u32(equality_mask);
+                if(zero_bits > avail_bits)
+                    zero_bits = avail_bits;
 
-                if (c_val == proc_byte && c_runLen + matched_bits <= 256 * 8)
-                    c_runLen += matched_bits;
+                uint32_t tmp_rlen = c_runLen + zero_bits;
+                if (tmp_rlen <= 255)
+                    c_runLen = tmp_rlen;
                 else
                 {
-                    comp_ptr[0] = c_runLen / 8 - 1;
+                    comp_ptr[0] = c_runLen;
                     comp_ptr[1] = c_val;
                     comp_ptr += 2;
-                    c_runLen = matched_bits;
-                    c_val = proc_byte;
+
+                    c_runLen = zero_bits;
                 }
+                avail_bits -= zero_bits;
+                equality_mask >>= zero_bits;
 
-                avail_bits -= matched_bits;
-                cur_val >>= matched_bits;
-                equality_mask >>= matched_bits;
+                if (avail_bits > 0){
+                    //if (c_runLen > 0)
+                    {
+                        comp_ptr[0] = c_runLen;
+                        comp_ptr[1] = c_val;
+                        comp_ptr += 2;
+
+                        c_runLen = 0;
+                        c_val = raw_Data[1 - avail_bits];
+                    }
+                    avail_bits--;
+                    equality_mask >>=1;
+                }
             }
-        }
-    }
-
-    cur_val = next_val;
-    uint64_t cur_val_shifted = (cur_val >> 8);
-    uint64_t equality_mask = cur_val_shifted ^ cur_val;
-    //count matched parts
-    if (equality_mask == 0)
-    {
-        uint8_t proc_byte = cur_val;
-        if (c_val == proc_byte && c_runLen + 64 <= 256 * 8)
-            c_runLen += 64;
-        else
-        {
-            comp_ptr[0] = c_runLen / 8 - 1;
-            comp_ptr[1] = c_val;
-            comp_ptr += 2;
-            //last write, no need to save values further
-            c_runLen = 64;
-            c_val = proc_byte;
-        }
-    }
-    else
-    {
-        int32_t avail_bits = 64;
-        for (; avail_bits > 0;)
-        {
-            uint8_t proc_byte = cur_val;
-            int32_t matched_bits = __tzcnt_u64(equality_mask) & ~7;
-            matched_bits += 8;
-            if (matched_bits > avail_bits)
-                matched_bits = avail_bits;
-
-            if (c_val == proc_byte && c_runLen + matched_bits <= 256 * 8)
-                c_runLen += matched_bits;
-            else
-            {
-                comp_ptr[0] = c_runLen / 8 - 1;
-                comp_ptr[1] = c_val;
-                comp_ptr += 2;
-
-                c_runLen = matched_bits;
-                c_val = proc_byte;
-            }
-
-            avail_bits -= matched_bits;
-            cur_val >>= matched_bits;
-            equality_mask >>= matched_bits;
-        }
+        raw_Data += 32;
     }
     if (c_runLen != 0)
     {
-        comp_ptr[0] = c_runLen / 8 - 1;
+        comp_ptr[0] = c_runLen - 1;
         comp_ptr[1] = c_val;
         comp_ptr += 2;
     }
